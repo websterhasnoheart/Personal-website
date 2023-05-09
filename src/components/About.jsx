9e2a6de86c0629ef208356f57aba495cfb09b960
webster <websterchan1998@gmail.com>
2023-05-09 15:10:07 +0200

    upload complete website

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,11 +6,12 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  <Tilt className="xs:w-[250px] w-full">
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
     >
       <div
         options={{
@@ -18,15 +19,15 @@ const ServiceCard = ({ index, title, icon }) => (
           scale: 1,
           speed: 450,
         }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
       >
         <img
           src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
+          alt="web-development"
+          className="w-16 h-16 object-contain"
         />
 
-        <h3 className='text-white text-[20px] font-bold text-center'>
+        <h3 className="text-white text-[20px] font-bold text-center">
           {title}
         </h3>
       </div>
@@ -34,35 +35,34 @@ const ServiceCard = ({ index, title, icon }) => (
   </Tilt>
 );
 
-
 const About = () => {
   return (
-    <>
+    <div className="w-full px-4 lg:px-0 flex flex-col items-center">
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>
           Introduction
         </p>
         <h2 className={styles.sectionHeadText}>
-          Overview </h2>
+          Overview
+        </h2>
       </motion.div>
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        className="mt-4 text-secondary text-[17px] max-w-7xl leading-[30px]"
       >
-        Object-oriented, self-motivated and organized software engineer. 
-        Works at fast pace to meet tight deadlines and always ready for new challenges. 
+        Object-oriented, self-motivated and organized software engineer.  Works at fast pace to meet tight deadlines and always ready for new challenges. 
         Enthusiastic team player ready to contribute to business success with dedicated attitude 
         and innovation of technology.
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className="mt-20 flex justify-center flex-wrap gap-20" style={{ width: "100vw" }}>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default About
+export default About;
